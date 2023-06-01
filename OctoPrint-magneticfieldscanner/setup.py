ee42cb7ab2048d4f602ce70d26f0597d3e63e5d8
Paweł Frączkiewicz <fraczpa@gmail.com>
2023-06-01 13:04:41 +0200

    informacja o dodaniu

--- OctoPrint-magneticfieldscanner/setup.py
+++ OctoPrint-magneticfieldscanner/setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "mynewplugin"
+plugin_identifier = "magneticfieldscanner"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_%s" % plugin_identifier
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-MyNewPlugin"
+plugin_name = "OctoPrint-magneticfieldscanner"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1"
@@ -27,7 +27,7 @@ plugin_author = "TODO"
 plugin_author_email = "todo@example.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/you/OctoPrint-MyNewPlugin"
+plugin_url = "https://github.com/Domix14/magneticfieldscanner"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
